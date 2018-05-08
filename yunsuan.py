87f1cb6d13785f89c7e9725b8d1a3c3146f35d8b
OCC111 <15600686330@163.com>
2018-05-08 13:36:07 +0800

    此内容为开放资源，禁止贩卖

---
+++ yunsuan.py
@@ -0,0 +1,26 @@
+a=21
+b=10
+c=0
+c=a+b
+print("1-c的值:",c)
+c=a-b
+print("2-c的值:",c)
+c=a*b
+print("3-c的值:",c)
+c=a/b
+print("4-c的值:",c)
+c=a%b
+print("5-c的值:",c)
+#修改变量 a、b、c
+a=2
+b=3
+c=a**b
+print("6-c的值:",c)
+a=10
+b=5
+c=a//b
+print("7-c的值:",c)
+# wtm222
+# python
+# python
+# python
